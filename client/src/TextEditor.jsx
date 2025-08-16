8ea35be0b4a88a580cbf633a785e00fe64a9eb6a
Abhishek Deshmukh <abhishekdeshmukh74@gmail.com>
2025-08-16 11:38:31 +0530

    Added styles to editor

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,19 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useState, useCallback } from 'react';
 import Quill from 'quill';
 
-import "quill/dist/quill.snow.css";
+import 'quill/dist/quill.snow.css';
+
+const SAVE_INTERVAL_MS = 2000;
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: 'ordered' }, { list: 'bullet' }],
+  ['bold', 'italic', 'underline'],
+  [{ color: [] }, { background: [] }],
+  [{ script: 'sub' }, { script: 'super' }],
+  [{ align: [] }],
+  ['image', 'blockquote', 'code-block'],
+  ['clean'],
+];
 
 function TextEditor() {
 
-    useEffect(() => {
-        new Quill('#editor', {
-            theme: 'snow',
-        });
-    }, []);
+  const [quill, setQuill] = useState();
 
-    return (
-        <div id='editor'></div>
-    )
+  const editorRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+
+    wrapper.innerHTML = '';
+    const editor = document.createElement('div');
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: 'snow',
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    q.disable();
+    q.setText('Loading...');
+    setQuill(q);
+  }, []);
+
+  return <div id='' className='container' ref={editorRef}></div>;
 }
 
-export default TextEditor
+export default TextEditor;
